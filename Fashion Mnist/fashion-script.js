078106adc7fd9dbb301b55d2e3260c7e2518f69b
Prikshit Singla <46683777+prikshit-2000@users.noreply.github.com>
2021-06-17 21:42:51 +0530

    Update fashion-script.js

--- Fashion Mnist/fashion-script.js
+++ Fashion Mnist/fashion-script.js
@@ -114,7 +114,7 @@ function init() {
 }
  async function getloadedModel() {
     
-   const  model = await tf.loadLayersModel('./my_model.json');
+   const  model = await tf.loadModel('./my_model.json');
     
   
 
